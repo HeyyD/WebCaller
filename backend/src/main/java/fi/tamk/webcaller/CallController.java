184afe2ee16a07b1971237c8defc7ee12dcb113e
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-16 18:25:09 +0200

    Modify CallController
    - (call): Add new method

--- backend/src/main/java/fi/tamk/webcaller/CallController.java
+++ backend/src/main/java/fi/tamk/webcaller/CallController.java
@@ -1,5 +1,12 @@
 package fi.tamk.webcaller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CallController {
 
+    @RequestMapping("/greeting")
+    public String call(){
+        return null;
+    }
 }
