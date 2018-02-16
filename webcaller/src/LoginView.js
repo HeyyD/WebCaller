f9ae27c6e7ed252b12a26bb27f78972baaa4d086
HeyyD <heidi.hati@cs.tamk.fi>
2018-02-16 15:34:19 +0200

    Modify LoginView
    -Add constructor
    -Add function login
    -(render) add onClick to button

--- webcaller/src/LoginView.js
+++ webcaller/src/LoginView.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import './styles/LoginView.css'
 
 class LoginView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+  }
+
+  login(event) {
+    event.preventDefault();
+  }
+
   render(){
     return(
       <form>
@@ -10,7 +20,7 @@ class LoginView extends Component {
           <input type="text"/>
           <label>Password:</label>
           <input type="text"/>
-          <button>Login</button>
+          <button onClick={this.login}>Login</button>
         </div>
       </form>
     );
