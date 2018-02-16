8faf68f61a84ce30bb983f30a06727ad4163e7e2
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-16 19:26:53 +0200

    Modify CallController
    -(makeCall): Add new empty method.

--- backend/src/main/java/fi/tamk/webcaller/CallController.java
+++ backend/src/main/java/fi/tamk/webcaller/CallController.java
@@ -4,6 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.MessageFactory;
+import com.twilio.sdk.resource.instance.Message;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import com.twilio.sdk.resource.factory.CallFactory;
+import com.twilio.sdk.resource.instance.Call;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CallController {
@@ -14,6 +25,31 @@ public class CallController {
 
     @RequestMapping("/testcall")
     public String call(){
+        sendSMS();
         return "testcall";
     }
+
+    public void sendSMS() {
+        try {
+            TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+
+            // Build a filter for the MessageList
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("Body", "Hello, World!"));
+            params.add(new BasicNameValuePair("To", "+358400366613")); //Add real number here
+            params.add(new BasicNameValuePair("From", TWILIO_NUMBER));
+
+            MessageFactory messageFactory = client.getAccount().getMessageFactory();
+            Message message = messageFactory.create(params);
+            System.out.println(message.getSid());
+        }
+        catch (TwilioRestException e) {
+            System.out.println(e.getErrorMessage());
+        }
+    }
+
+    public void makeCall() {
+        
+    }
+
 }
