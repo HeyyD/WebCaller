d1528962a347a0e7e846907aadddca5a92d9c2bd
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-16 18:30:39 +0200

    Modify pom.xml
    - (spring-web): Add new dependency

--- backend/src/main/java/fi/tamk/webcaller/CallController.java
+++ backend/src/main/java/fi/tamk/webcaller/CallController.java
@@ -1,12 +1,13 @@
 package fi.tamk.webcaller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CallController {
 
     @RequestMapping("/greeting")
     public String call(){
-        return null;
+        return "Hello world!";
     }
 }
