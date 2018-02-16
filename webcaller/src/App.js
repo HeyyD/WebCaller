79614f09d8b6d500bc0604ce9c5d515502d0829e
HeyyD <heidi.hati@cs.tamk.fi>
2018-02-16 13:32:12 +0200

    -Add style LoginView.css

--- webcaller/src/App.js
+++ webcaller/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import LoginView from './LoginView';
 
 class App extends Component {
--- webcaller/src/LoginView.js
+++ webcaller/src/LoginView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './styles/LoginView.css'
 
 class LoginView extends Component {
   render(){
