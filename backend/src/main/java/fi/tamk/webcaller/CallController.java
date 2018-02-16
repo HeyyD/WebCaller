0f13550cd57a477b6338b298152b15c04999a7ee
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-16 19:15:00 +0200

    Modify CallController
    -(ACCOUNT_SID): Add new String variable
    -(AUTH_TOKEN): Add new String variable
    -(TWILIO_NUMBER): Add new String variable

--- backend/src/main/java/fi/tamk/webcaller/CallController.java
+++ backend/src/main/java/fi/tamk/webcaller/CallController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CallController {
 
+    public static final String ACCOUNT_SID = "AC4a59caf3e6d9cb3e8aa3920d03447b04";
+    public static final String AUTH_TOKEN = "0e337a0dd91ddb837ed3d4370ade02e9";
+    public static final String TWILIO_NUMBER = "+358248092145";
+
     @RequestMapping("/testcall")
     public String call(){
         return "testcall";
