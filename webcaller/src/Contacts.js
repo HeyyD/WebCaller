4e1eadba21879679db68282bfe5410a7bc9b9530
HeyyD <heidi.hati@cs.tamk.fi>
2018-02-20 17:39:32 +0200

    Modify Contacts
    -(render) Fix html syntax
    -(Contacts) Add constructor

--- webcaller/src/Contacts.js
+++ webcaller/src/Contacts.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 
+import './styles/MainView.css'
+
 class Contacts extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     //TODO: Contact class
     return (
-      <table>
-        <tr>
-          <th>Phone</th>
-          <th>Name</th>
-          <th>Company</th>
-        </tr>
-        <tr>
-          <td>+35840666666</td>
-          <td>Banaani Tom</td>
-          <td>PornHub</td>
-          <td><button>Call</button></td>
-        </tr>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Phone</th>
+              <th>Name</th>
+              <th>Company</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>+35840666666</td>
+              <td>Banaani Tom</td>
+              <td>PornHub</td>
+              <td><button>Call</button></td>
+            </tr>
+          </tbody>
+        </table>
+        <button>Add Contact</button>
+      </div>
     );
   }
 }
