84de38b19a6e50cd359e7124397bf32c546043ab
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-27 14:55:39 +0200

    Modify MainLayout
    -(MainLayout): Modify returns real content

--- webcaller/client/layouts/MainLayout.jsx
+++ webcaller/client/layouts/MainLayout.jsx
@@ -2,5 +2,16 @@ import React from 'react';
 import AccountsUI from '../../AccountsUI.jsx';
 
 export const MainLayout = ({content}) => (
-    <p></p>
+    <div className="main-layout">
+    <header>
+            <h2>WebCaller</h2>
+            <nav>
+                <a href="/">User</a>
+                <AccountsUI />
+            </nav>
+    </header>
+    <main>
+        {content}
+    </main>
+</div>
 )
