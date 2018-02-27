000aff704b03694ad8b86a6b73fc958c947e3dd6
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-27 07:23:26 +0200

    Modify CallController bugfix
    -(makeCall): Modify bugfix

--- backend/src/main/java/fi/tamk/webcaller/CallController.java
+++ backend/src/main/java/fi/tamk/webcaller/CallController.java
@@ -1,13 +1,12 @@
 package fi.tamk.webcaller;
 
+import com.twilio.sdk.verbs.Dial;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.TwilioRestException;
-import com.twilio.sdk.resource.factory.MessageFactory;
-import com.twilio.sdk.resource.instance.Message;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import com.twilio.sdk.resource.factory.CallFactory;
@@ -23,6 +22,7 @@ public class CallController {
     public static final String AUTH_TOKEN = "0e337a0dd91ddb837ed3d4370ade02e9";
     public static final String TWILIO_NUMBER = "+358248092145";
 
+
     @RequestMapping("/testcall")
     public String call(){
         makeCall();
@@ -35,11 +35,14 @@ public class CallController {
             TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
 
             List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("Url", "https://brodan.biz/call.xml"));
+            Dial dial = new Dial();
+            System.out.println(dial.toString());
+            params.add(new BasicNameValuePair("Url", "http://koti.tamk.fi/~c6samhau/call.xml"));
             params.add(new BasicNameValuePair("To", "+358400366613")); //Add real number here
             params.add(new BasicNameValuePair("From", TWILIO_NUMBER));
 
             CallFactory callFactory = client.getAccount().getCallFactory();
+
             Call call = callFactory.create(params);
         }
         catch (TwilioRestException e) {
