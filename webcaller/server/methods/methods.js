3df00547795ad68ddc4126bff6676466f1836c0f
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-28 12:25:08 +0200

    Modify methods.js added new constant variables
    -(express): Add new constant, contians express
    -(app): Add new constant, usage of express

--- webcaller/server/methods/methods.js
+++ webcaller/server/methods/methods.js
@@ -1,7 +1,8 @@
 var accountSid = 'AC4a59caf3e6d9cb3e8aa3920d03447b04'; // Your Account SID from www.twilio.com/console
 var authToken = '0e337a0dd91ddb837ed3d4370ade02e9';   // Your Auth Token from www.twilio.com/console
 
-
+const express = require('express');
+const app = express();
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
