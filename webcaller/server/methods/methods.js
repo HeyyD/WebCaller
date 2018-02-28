4d9d31baab2332154e8ee5eabb6155640825aeca
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-28 12:23:50 +0200

    Add express new package

--- webcaller/server/methods/methods.js
+++ webcaller/server/methods/methods.js
@@ -1,12 +1,17 @@
 var accountSid = 'AC4a59caf3e6d9cb3e8aa3920d03447b04'; // Your Account SID from www.twilio.com/console
 var authToken = '0e337a0dd91ddb837ed3d4370ade02e9';   // Your Auth Token from www.twilio.com/console
 
+
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
+
 Meteor.methods({
     makeCall(number){
-        console.log(client);
+        const twiml = new VoiceResponse();
+        twiml.say({ voice: 'alice' }, 'hello world!');
+
         client.calls.create({
           url: 'http://koti.tamk.fi/~c6samhau/call.xml',
           to: number,
