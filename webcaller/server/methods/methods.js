ca35e69c775f9ac6c54285c461f0fda15f3e9f1a
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-02-28 08:32:29 +0200

    Modify methods.js testing new package

--- webcaller/server/methods/methods.js
+++ webcaller/server/methods/methods.js
@@ -1,6 +1,19 @@
+var accountSid = 'AC4a59caf3e6d9cb3e8aa3920d03447b04'; // Your Account SID from www.twilio.com/console
+var authToken = '0e337a0dd91ddb837ed3d4370ade02e9';   // Your Auth Token from www.twilio.com/console
+
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+
+
 
 Meteor.methods({
     makeCall(number){
-        
+        client.messages.create({
+            body: 'Hello from Node',
+            to: '+358400366613',  // Text this number
+            from: '+358248092145' // From a valid Twilio number
+        })
+        .then((message) => console.log(message.sid));        
     }
 });
