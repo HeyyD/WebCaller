3bca7e0383aedec01f3ec076604f010006816b4b
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-03-03 11:22:34 +0200

    Modify ProjectPage new method render
    -(render): Add new method

--- webcaller/client/projects/ProjectPage.jsx
+++ webcaller/client/projects/ProjectPage.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 
 export default class ProjectPage extends React.Component {
 
-    
+    render(){
+        return(
+            <p>Hello world!</p>
+
+        );
+    }
 
 
 }
