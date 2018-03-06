8dd01a92913e3e36a77cb089b6e8499456f8da8c
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-03-06 18:10:01 +0200

    Modify ProjectPage renders form

--- webcaller/client/projects/ProjectPage.jsx
+++ webcaller/client/projects/ProjectPage.jsx
@@ -5,7 +5,12 @@ export default class ProjectPage extends React.Component {
 
     render(){
         return(
-            <p>Hello world!</p>
+            <form>
+                <label>Project name:</label>
+                <input type="text" name="projectName" onChange={console.log("hi")}/>
+                <label>Description:</label>
+                <input type="text" name="projectDescription" onChange={console.log("hi")}/>
+            </form>
 
         );
     }
