13ff1c31e97296069282980a2b5c5ea893ef0dfb
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-03-06 18:11:51 +0200

    Add ProjectForm new component
    -(ProjectForm): Add new component

---
+++ webcaller/client/contacts/ProjectForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ProjectForm extends React.Component {
+
+    render(){
+        return(
+            <form>
+                <label>Project name:</label>
+                <input type="text" name="projectName" onChange={console.log("hi")}/>
+                <label>Description:</label>
+                <input type="text" name="projectDescription" onChange={console.log("hi")}/>
+            </form>
+
+        );
+    }
+
+
+}
--- webcaller/client/projects/ProjectPage.jsx
+++ webcaller/client/projects/ProjectPage.jsx
@@ -5,12 +5,7 @@ export default class ProjectPage extends React.Component {
 
     render(){
         return(
-            <form>
-                <label>Project name:</label>
-                <input type="text" name="projectName" onChange={console.log("hi")}/>
-                <label>Description:</label>
-                <input type="text" name="projectDescription" onChange={console.log("hi")}/>
-            </form>
+            
 
         );
     }
