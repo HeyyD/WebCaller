e0dcfac91c9889e22013c1aa18f5379a8bcbf425
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-03-06 18:13:09 +0200

    Modify ProjectPage renders ProjectForm

--- webcaller/client/projects/ProjectPage.jsx
+++ webcaller/client/projects/ProjectPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ProjectForm from './ProjectForm.jsx';
 
 export default class ProjectPage extends React.Component {
 
     render(){
         return(
-            
-
+            <ProjectForm />
         );
     }
 
