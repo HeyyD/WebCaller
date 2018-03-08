51a6bdbc0d24500141e206bc6e5e40e008b1b041
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-03-08 16:09:42 +0200

    Add ProjectSingle new component

---
+++ webcaller/client/projects/ProjectSingle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ProjectSingle extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(<p>Hello world!</p>);
+    }
+
+}
