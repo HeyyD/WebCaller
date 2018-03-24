8fafa73c4b0243dfa9a373b51192edcc9e8cbe02
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-03-24 09:53:23 +0200

    Modify AgentsForm render method
    -(render): Modify now renders form

--- webcaller/client/agents/AgentsForm.jsx
+++ webcaller/client/agents/AgentsForm.jsx
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 
 export default class AgentsForm extends React.Component {
     render(){
-        return <p> form </p>
+        return(
+            <form>
+                <div>
+                    <div>
+                        <label>Username:</label></div>
+                    <div>
+                        <input type="text" 
+                            name="username" 
+                            ref="username" 
+                            onChange={this.handleChange}/>
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                    </div>
+                        <input type="text" 
+                        name="password" 
+                        ref="password" 
+                        onChange={this.handleChange}/>
+                    <button onClick={this.addProject}>Add Project</button>
+                </div>
+            </form>
+        );
     }
 }
