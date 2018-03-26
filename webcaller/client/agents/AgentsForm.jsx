de4e73ab5d91c69b3863e946d3f22644297f6efc
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-03-26 19:25:13 +0300

    Modify AgentsForm removed consolelog from render
    -(render): Modify removed unneccessary consolelog

--- webcaller/client/agents/AgentsForm.jsx
+++ webcaller/client/agents/AgentsForm.jsx
@@ -34,7 +34,6 @@ export default class AgentsForm extends React.Component {
     }
 
     render(){
-        console.log(Meteor.users.find().fetch());
         return(
             <form>
                 <div>
