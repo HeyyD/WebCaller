f8ce92d3af27c70adf794e1393c5dc6d3031cebe
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-03-28 14:03:37 +0300

    Modify CallListPage.jsx add new methods
    -(constructor): Add new method
    -(lists): Add new method
    -(render): Add new method

--- webcaller/client/calllists/CallListPage.jsx
+++ webcaller/client/calllists/CallListPage.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+CallLists = new Mongo.Collection("calllists");
+
 export default class CallListPage extends React.Component {
   
+  constructor(props){
+    super(props);
+        /*this.state = {
+            subscription: {
+                projects: Meteor.subscribe("userProjects")
+            }
+        }*/
+    }
+
+    /*componentWillUnmount(){
+        this.state.subscription.projects.stop();
+    }*/
+
+    lists(){
+        return CallLists.find().fetch();
+    }
+
+    render(){
+        console.log(this.lists())
+        return(
+            <div>
+                <CallListForm />
+            </div>
+        );
+    }
 }
