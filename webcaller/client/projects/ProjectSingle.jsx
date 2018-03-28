b7adc371c0f0bc50184eb94cc5a8155f5e294d7e
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-28 23:19:06 +0300

    Add new function deleteProject
    -(ProjectSingle): Add function deleteProject

--- webcaller/client/projects/ProjectSingle.jsx
+++ webcaller/client/projects/ProjectSingle.jsx
@@ -5,10 +5,20 @@ export default class ProjectSingle extends React.Component {
 
     constructor(props){
         super(props);
+        this.deleteProject = this.deleteProject.bind(this);
+    }
+
+    deleteProject() {
+
     }
 
     render(){
-        return(<li><a href={"projects/"+this.props.project._id}>{this.props.project.name}</a><button>X</button></li>);
+        return(
+        <li>
+            <a href={"projects/"+this.props.project._id}>{this.props.project.name}</a>
+            <button>X</button>
+        </li>
+        );
     }
 
 }
