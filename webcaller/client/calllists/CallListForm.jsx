8b85340dd6d6fd1a98025540cc6e9aaae4c43663
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-03-28 13:19:38 +0300

    Modify CallListForm.jsx add render method
    -(render): Add new method

--- webcaller/client/calllists/CallListForm.jsx
+++ webcaller/client/calllists/CallListForm.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class CallListPage extends React.Component {
+export default class CallListForm extends React.Component {
 
     
+    render(){
+        return(
+            <form>
+                <div>
+                    <div>
+                        <label>Call list name:</label></div>
+                    <div>
+                        <input type="text" 
+                            name="projectName" 
+                            ref="projectName" 
+                            onChange={this.handleChange}/>
+                    </div>
+                    <div>
+                        <label>Description:</label>
+                    </div>
+                    <div>
+                        <textarea className="desc" 
+                            name="projectDescription" 
+                            ref="projectDescription"
+                            onChange={this.handleChange}/>
+                    </div>
+                    <button onClick={this.addList}>Add List</button>
+                </div>
+            </form>
+        );
+    }
 }
