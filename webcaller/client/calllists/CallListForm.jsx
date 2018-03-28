e2adcfe85bf2ab8f31f9ed701802d229b9f7c1cf
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-03-28 13:41:32 +0300

    Modify CallListForm.jsx add new method
    -(addList): Add new method

--- webcaller/client/calllists/CallListForm.jsx
+++ webcaller/client/calllists/CallListForm.jsx
@@ -6,19 +6,35 @@ export default class CallListForm extends React.Component {
     constructor(props){
         super(props);
         this.state = ({
-            projectName: "",
-            projectDescription: "",
-            callLists: [],
-            projectAgents: []
+            listName: "",
+            listDescription: "",
+            contacts: []
         });
 
         this.handleChange = this.handleChange.bind(this);
-        this.addProject = this.addProject.bind(this);
+        this.addList = this.addList.bind(this);
     }
 
     handleChange(event){
         this.setState({[event.target.name]: event.target.value});
     }   
+
+    addList(event){
+        let list = {
+            name: this.state.listName,
+            description: this.state.listDescription,
+            contacts: this.state.contacts
+        };
+        Meteor.call('addCallList', list);
+        this.refs.listName.value = "";
+        this.refs.listDescription.value = "";
+        this.setState({
+            listName: "",
+            listDescription: "",
+            contacts: []
+        })
+        event.preventDefault();
+    }
     
     render(){
         return(
@@ -28,8 +44,8 @@ export default class CallListForm extends React.Component {
                         <label>Call list name:</label></div>
                     <div>
                         <input type="text" 
-                            name="projectName" 
-                            ref="projectName" 
+                            name="listName" 
+                            ref="listName" 
                             onChange={this.handleChange}/>
                     </div>
                     <div>
@@ -37,8 +53,8 @@ export default class CallListForm extends React.Component {
                     </div>
                     <div>
                         <textarea className="desc" 
-                            name="projectDescription" 
-                            ref="projectDescription"
+                            name="listDescription" 
+                            ref="listDescription"
                             onChange={this.handleChange}/>
                     </div>
                     <button onClick={this.addList}>Add List</button>
