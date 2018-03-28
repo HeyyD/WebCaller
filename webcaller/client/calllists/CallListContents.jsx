c3e0476401d5679b08daf8324407f72234d05552
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-03-28 16:11:57 +0300

    Modify CallListContents.jsx add methods
    -(constructor): Modify method, init state
    -(componentWillUnmount): Add method
    -(callLists): Add method
    -(render): Add return

--- webcaller/client/calllists/CallListContents.jsx
+++ webcaller/client/calllists/CallListContents.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
-export default class CallListContents extends React.Component {
+export default class CallListContents extends TrackerReact(React.Component) {
 
     constructor(props){
         super(props);
-        
+        this.state = {
+            subscription: {
+                lists: Meteor.subscribe("callLists")
+            }
+        }
+    }
+    
+    componentWillUnmount(){
+        this.state.subscription.lists.stop();
+    }
+
+    callLists(){
+        return CallLists.find({_id: this.props.id}).fetch();
     }
 
     render(){
+        console.log(this.callLists());
+        console.log(this.props.id);
+        return(
+            <div>
+                <ul>
+                    {this.callLists().map( (list)=>{
+                        return <p>Hello World</p>;
+                    })}                
+                </ul>
+            </div>
+        );
     }
 
 }
