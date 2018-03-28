eab9f71a010acd98cb90e17e9b83268e094cefe0
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-03-28 15:48:24 +0300

    Add CallListContents.jsx new component
    -(CallListContents): Add new component

---
+++ webcaller/client/calllists/CallListContents.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class CallListContents extends React.Component {
+
+    constructor(props){
+        super(props);
+        
+    }
+
+    render(){
+    }
+
+}
