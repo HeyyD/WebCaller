bda6c461d16f4dbc999d02f644d412ff1980fde9
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-03-28 16:16:37 +0300

    Modify routes.jsx modify /calllists/:id route
    -(/calllists/:id): Modify content of route

--- webcaller/client/routes.jsx
+++ webcaller/client/routes.jsx
@@ -4,6 +4,7 @@ import Contacts from './contacts/Contacts.jsx';
 import ProjectPage from './projects/ProjectPage.jsx';
 import AgentsPage from './agents/AgentsPage.jsx';
 import CallListPage from './calllists/CallListPage.jsx';
+import CallListContents from './calllists/CallListContents.jsx';
 
 import {MainLayout} from './layouts/MainLayout.jsx';
 
@@ -51,7 +52,7 @@ FlowRouter.route('/calllists', {
 FlowRouter.route('/calllists/:id', {
     action(params) {
         mount(MainLayout, {
-            content: (<p>{params.id}</p>)
+            content: (<CallListContents id={params.id}/>)
         })
     }
 });
