24d388fc3414c62c7b3e967217140ca1342757f7
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-04 13:03:07 +0300

    Modify package-lock.json and package.json
    - Add new component for parsing Excel files to JSON

--- webcaller/client/calllists/ContactSingle.jsx
+++ webcaller/client/calllists/ContactSingle.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ExcelReader from './ExcelReader.jsx';
 
 export default class ContactSingle extends React.Component {
 
@@ -9,22 +10,27 @@ export default class ContactSingle extends React.Component {
 
     readContact(){
         let keys = [];
+        let i = 0;
         for (let [key, value] of Object.entries(this.props.contact)) {
-            keys.push(<tr>
+            keys.push(<tr key={i}>
                         <td>{key}:</td> 
                         <td>{value}</td>
                     </tr>);
+            i++;
         }
         return keys;
     }
 
     render(){
         let keys = this.readContact();
-
+//{keys}
         return(
-            <div>
+            <table>
+                <tbody>
                 {keys}
-            </div>
+                <ExcelReader/>
+                </tbody>
+            </table>
         );
     }
 
