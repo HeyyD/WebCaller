7b5208ec2c1386a16f63875a28643ee2744f887e
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-04 13:05:48 +0300

    Modify ExcelReader.jsx remove old methods
    -(all methods): Removed to replace with new ones

--- webcaller/client/calllists/ExcelReader.jsx
+++ webcaller/client/calllists/ExcelReader.jsx
@@ -1,34 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as XLSX from 'xlsx';
 
 export default class ExcelReader extends React.Component {
 
-    constructor(props){
-        super(props);
-    }
-
-    readFile(){
-        
-        var name = file.name;
-        const reader = new FileReader();
-        reader.onload = (evt) => { //evt = on_file_select event
-            /* Parse data */
-            const bstr = evt.target.result;
-            const wb = XLSX.read(bstr, {type:'binary'});
-            /* Get first worksheet */
-            const wsname = wb.SheetNames[0];
-            const ws = wb.Sheets[wsname];
-            /* Convert array of arrays */
-            const data = XLSX.utils.sheet_to_csv(ws, {header:1});
-            /* Update state */
-            console.log("Data>>>"+data);
-        };
-        console.log('name = ' + name);
-        console.log(reader.readAsBinaryString(file));
-    }
-
-    render(){
-        return <tr><td>{this.readFile()}</td></tr>;
-    }
 }
