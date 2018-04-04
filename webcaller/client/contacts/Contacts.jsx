fff668c0de84ce1ffc69ad37f5f3becdc4030a50
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-04 12:46:00 +0300

    Modify ContactList component
    -(ContactList): Move ContactList component from Contacts to new file

--- webcaller/client/contacts/Contacts.jsx
+++ webcaller/client/contacts/Contacts.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ContactList from './ContactList';
 
 import './styles/AddContact.css';
 
@@ -59,52 +60,3 @@ class Contacts extends Component {
   }
 }
 export default Contacts;
-
-class ContactList extends Component {
-
-  constructor(props){
-    super(props);
-    this.createContactList = this.createContactList.bind(this);
-  }
-
-  makeCall(number){
-    Meteor.call('makeCall', number, (error) => {
-      console.log('hi!');
-    });
-  }
-
-  createContactList(){
-    let contactList = this.props.contacts;
-    let tableRows = [];
-    
-    for(let i = 0; i < contactList.length; i++) {
-      tableRows.push(<tr key={i}>
-                      <td>{contactList[i].phone}</td>
-                      <td>{contactList[i].name}</td>
-                      <td>{contactList[i].company}</td>
-                      <td><button onClick={ () => this.makeCall(contactList[i].phone)}>Call</button></td>
-                    </tr>);
-    }
-    return tableRows;
-  }
-
-  render(){    
-    return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Phone</th>
-              <th>Name</th>
-              <th>Company</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.createContactList()}
-          </tbody>
-        </table>
-        <button id="addContactButton" onClick={this.props.onClick}>Add Contact</button>
-    </div>
-    );
-  }
-}
