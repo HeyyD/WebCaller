49c4499cf59f91ad512bb64413aea6f5890a9d9c
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-04 13:17:05 +0300

    Modify ExcelReader.jsx add readFile method
    -(readFile): Add new method for parsing Excel file to JSON

--- webcaller/client/calllists/ExcelReader.jsx
+++ webcaller/client/calllists/ExcelReader.jsx
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom';
 
 export default class ExcelReader extends React.Component {
 
+    constructor(props) {
+        super(props);
+    }
+
+    readFile(){
+        let objectData;
+        convertExcel = require('excel-as-json').processFile;
+        convertExcel('../../numbers.xlsx', null, null, (error, data) => {
+            if(error) {
+                console.log('error');
+            }else{
+                objectData = data;
+            }
+        });
+    }
 }
