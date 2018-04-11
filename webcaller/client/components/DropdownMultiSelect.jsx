4f2c0a7f46ddf8ed4d383a7899603f7535be10d3
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-11 14:12:28 +0300

    Add new component DropdownMultiselect
    -(DropdownMultiselect): New component DropdownMultiselect

---
+++ webcaller/client/components/DropdownMultiSelect.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class DropdownMultiSelect extends Component {
+  render() {
+    return(
+      <p>Dropdown multiselect</p>
+    );
+  }
+}
+export default DropdownMultiSelect;
