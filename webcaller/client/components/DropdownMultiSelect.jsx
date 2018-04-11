3800c8dca371ff6dcf695b7bc1cf24b40e8a99e8
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-11 22:18:02 +0300

    Add new function itemSelected
    -(DropdownMultiselect): New function itemSelected
    -(DropdownMultiselect): New function updateMenu

--- webcaller/client/components/DropdownMultiSelect.jsx
+++ webcaller/client/components/DropdownMultiSelect.jsx
@@ -4,25 +4,49 @@ class DropdownMultiSelect extends Component {
 
   constructor(props) {
     super(props);
+    this.itemSelected = this.itemSelected.bind(this);
+    this.updateMenu = this.updateMenu.bind(this);
+
     this.state = {
       selected: [],
-      unselected: []
+      unselected: [],
+      options: []
     }
   }
 
   componentWillMount() {
     for(let o of this.props.options) {
-      this.state.unselected.push(<option>{o}</option>);
+      this.state.unselected.push(o);
     }
+    this.updateMenu(this.state.unselected);
+  }
+
+  updateMenu(array) {
+    let temp = [];
+    for(let o of array) {
+      temp.push(<option>{o}</option>);
+    }
+
+    this.setState({
+      options: temp
+    });
+  }
+
+  itemSelected(event) {
+    this.state.selected.push(event.target.value);
+
+    let index = this.state.unselected.indexOf(event.target.value);
+    this.state.unselected.splice(index, 1);
+    this.updateMenu(this.state.unselected);
   }
 
   render() {
     return(
       <label>
-        {this.props.title}
+        <h5>{this.props.title}</h5>
         <div>
-          <select>
-            {this.state.unselected}
+          <select onChange={this.itemSelected}>
+            {this.state.options}
           </select>
         </div>
       </label>
