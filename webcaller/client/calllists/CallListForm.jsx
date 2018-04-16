02ad0986e7c0c00354e7a2d79b727f641f9a5f8e
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-16 20:44:18 +0300

    Modify CallListForm.jsx add new method
    -(onDrop): Add new method to handle Excel file upload

--- webcaller/client/calllists/CallListForm.jsx
+++ webcaller/client/calllists/CallListForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import XLSX from 'xlsx';
+import Dropzone from 'react-dropzone'
 
 export default class CallListForm extends React.Component {
 
@@ -27,7 +29,36 @@ export default class CallListForm extends React.Component {
 
     handleChange(event){
         this.setState({[event.target.name]: event.target.value});
-    }   
+    }
+
+    onDrop(acceptedFiles, rejectedFiles) {
+        acceptedFiles.forEach(file => {
+            const reader = new FileReader();
+            const rABS = !!reader.readAsBinaryString;
+            reader.onload = (e) => {
+			// Parse data
+			const bstr = e.target.result;
+			const wb = XLSX.read(bstr, {type:rABS ? 'binary' : 'array'});
+			// Get first worksheet 
+			const wsname = wb.SheetNames[0];
+			const ws = wb.Sheets[wsname];
+			// Convert array of arrays
+            const data = XLSX.utils.sheet_to_json(ws, {header:1});
+            // Pass data to backend 
+            console.log(data);
+            Meteor.call('parseExcelData', data);
+			//this.setState({ data: data, cols: make_cols(ws['!ref']) });
+		};
+		if(rABS) {
+            reader.readAsBinaryString(file);
+        } else {
+            reader.readAsArrayBuffer(file);
+        }
+
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+        });
+    }
 
     addList(event){
         let list = {
