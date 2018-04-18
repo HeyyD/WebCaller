21a1345f8d394c5f92a3d42981c60a3ec19bc2b2
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-18 15:27:09 +0300

    Modify CallWindow
    -(render): Add user customer information

--- webcaller/client/contacts/CallWindow.jsx
+++ webcaller/client/contacts/CallWindow.jsx
@@ -10,7 +10,9 @@ class CallWindow extends Component {
     return(
       <div className="Call-window-outer">
         <div className="Call-window-inner">
-          <h1>Call window</h1>
+          <h1>Call</h1>
+          <h3>{this.props.customer.name}</h3>
+          <h3>{this.props.customer.company}</h3>
           <button onClick={this.props.onClick}>Hang up</button>
         </div>
       </div>
