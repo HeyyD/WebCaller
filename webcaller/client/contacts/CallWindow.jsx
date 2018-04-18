00b5397746902fe66399662b9e5a0c68c89f26af
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-18 14:40:24 +0300

    Modify CallWindow
    -(constructor): Add constructor

--- webcaller/client/contacts/CallWindow.jsx
+++ webcaller/client/contacts/CallWindow.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class CallWindow extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return(
       <div className="Call-window-outer">
         <div className="Call-window-inner">
           <h1>Call window</h1>
-          <button>Hang up</button>
+          <button onClick={this.props.onClick}>Hang up</button>
         </div>
       </div>
     );
