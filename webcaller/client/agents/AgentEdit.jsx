63025c0c858635fc060d93473e47aaf15e5dcb60
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-04-19 14:40:34 +0300

    Add AgentEdit
    -(AgentEdit): add new empty component

---
+++ webcaller/client/agents/AgentEdit.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+
+export default class AgentEdit extends TrackerReact(React.Component){
+  
+}
