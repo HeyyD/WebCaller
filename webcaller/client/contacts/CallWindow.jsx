5de9a17ff9ffe19b938f2700946818571fc029c0
HeyyD <heidi.hati@cs.tamk.fi>
2018-04-19 11:56:42 +0300

    Modify CallWindow
    -(render): Disable buttons
    -(styles): Modify styles

--- webcaller/client/contacts/CallWindow.jsx
+++ webcaller/client/contacts/CallWindow.jsx
@@ -12,7 +12,8 @@ class CallWindow extends Component {
       current: this.props.currentCustomer,
       customer : {
         name: this.props.contacts[this.props.currentCustomer].name,
-        company: this.props.contacts[this.props.currentCustomer].company
+        company: this.props.contacts[this.props.currentCustomer].company,
+        number: this.props.contacts[this.props.currentCustomer].number
       }
     }
   }
@@ -36,10 +37,15 @@ class CallWindow extends Component {
           <h1>Call</h1>
           <h3>{this.state.customer.name}</h3>
           <h3>{this.state.customer.company}</h3>
+          <h3>{this.state.customer.number}</h3>
           <div className="Call-window-button-container">
-            <input type="button" value="Previous"/>
+            <input  type="button"
+                    value="Previous"
+                    disabled={this.state.current === 0}/>
             <input type="button" value="Call"/>
-            <input type="button" value="Next"/>
+            <input  type="button"
+                    value="Next"
+                    disabled={this.state.current === (this.props.contacts.length - 1)}/>
           </div>
         </div>
       </div>
