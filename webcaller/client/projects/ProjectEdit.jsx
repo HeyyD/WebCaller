3c8eefd0e5d26adb715b0c690665f94c9726f2e4
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-04-19 14:24:20 +0300

    Modify ProjectEdit removed import
    -(import): Delete MultiSelect

--- webcaller/client/projects/ProjectEdit.jsx
+++ webcaller/client/projects/ProjectEdit.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
-import DropdownMultiSelect from '../components/DropdownMultiSelect';
-
 
 export default class ProjectEdit extends TrackerReact(React.Component) {
 
