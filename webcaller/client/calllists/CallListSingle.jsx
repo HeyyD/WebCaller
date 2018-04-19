8bdf2d08b5a923ee99b3a06798af3ce67953d910
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-19 08:47:02 +0300

    Modify CallListSingle.jsx add new method
    -(deleteCallList): Add new method for deleting call list

--- webcaller/client/calllists/CallListSingle.jsx
+++ webcaller/client/calllists/CallListSingle.jsx
@@ -5,10 +5,16 @@ export default class CallListSingle extends React.Component {
 
     constructor(props){
         super(props);
+        this.deleteCallList = this.deleteCallList.bind(this);
+    }
+
+    deleteCallList() {
+        Meteor.call('deleteCallList', this.props.list._id);      
     }
 
     render(){
-        return(<li><a href={"calllists/"+this.props.list._id}>{this.props.list.name}</a><button>X</button></li>);
+        return(<li><a href={"calllists/"+this.props.list._id}>{this.props.list.name}</a>
+        <button onClick={this.deleteCallList}>X</button></li>);
     }
 
 }
