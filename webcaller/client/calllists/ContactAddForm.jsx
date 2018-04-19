303f8065b18483d774577b376ac0e54b47b8ba6d
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-20 00:22:51 +0300

    Modify ContactAddForm.jsx add constructor

--- webcaller/client/calllists/ContactAddForm.jsx
+++ webcaller/client/calllists/ContactAddForm.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class ContactAddForm extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = ({
+            contacts: []
+        })
+        this.handleChange = this.handleChange.bind(this);
+        this.addContact = this.addContact.bind(this);
+    }
+
     
 
     render(){
