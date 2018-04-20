377f4d7b2dff85fcd206286b85c17bae3766918c
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-20 03:19:46 +0300

    Modify ContactAddForm.jsx add render method
    -(render): Returns complete form to add new contacts
    to CallLists database

--- webcaller/client/calllists/ContactAddForm.jsx
+++ webcaller/client/calllists/ContactAddForm.jsx
@@ -6,23 +6,52 @@ export default class ContactAddForm extends React.Component {
         super(props);
 
         this.state = ({
-            contacts: []
+            id: props.id,
+            contacts: props.contacts,
+            keys: Object.keys(props.contacts[0])
         })
         this.handleChange = this.handleChange.bind(this);
         this.addContact = this.addContact.bind(this);
-    }
-
-    componentWillReceiveProps(props){
-        this.setState({contacts: props.contacts});
     }
 
     handleChange(event){
         this.setState({[event.target.name]: event.target.value});
     }
 
-    
+    addContact(event){
+
+        let newContact = {};
+        this.state.keys.map( (key) => {
+            newContact[key] = this.state[key];
+        });
+        let newContacts = this.state.contacts;
+        newContacts.push(newContact);
+        Meteor.apply('insertContact', [this.state.id, newContacts]);
+
+        this.refs.form.reset();
+        event.preventDefault();
+    }
 
     render(){
 
+        return(
+            <form ref="form">
+                <div>
+                    {this.state.keys.map( (key) => {
+                        return(
+                        <div>
+                            <label>{key}</label>
+                        
+                            <input type="text" 
+                                name={key}
+                                ref={key}
+                                onChange={this.handleChange}/>
+                        </div>
+                        )
+                    })}
+                    <button onClick={this.addContact}>Add Contact</button>
+                </div>
+            </form>
+        );
     }
 }
